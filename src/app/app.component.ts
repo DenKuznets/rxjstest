f904e6524a93e617b8d79d0c65eb9d8105a81d85
Denis Kuznetsov <denis.kyznecov@gmail.com>
2024-01-12 14:23:50 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import {
+  Observable,
+  of,
+  map,
+  Subject,
+  asyncScheduler,
+  observeOn,
+  asapScheduler,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +22,16 @@ export class AppComponent implements OnInit {
   title = 'rxjstest';
 
   ngOnInit(): void {
-    const numbers$ = of(1, 2, 3);
-    numbers$.subscribe({
-      next: (value) =>
-        console.log('Observable emitted the next value: ' + value),
-      error: (err) => console.error('Observable emitted an error: ' + err),
-      complete: () =>
-        console.log('Observable emitted the complete notification'),
-    });
+    const observable = new Observable((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+    }).pipe(observeOn(asapScheduler));
+
+    console.log('before subscribe');
+
+    observable.subscribe((val) => console.log(val));
+
+    console.log('after subscribe');
   }
 }
