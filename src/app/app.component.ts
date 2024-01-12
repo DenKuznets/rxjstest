fbb653068b5e5ba90157f26fedcdb04999ef32b1
Denis Kuznetsov <denis.kyznecov@gmail.com>
2024-01-12 08:12:52 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjstest';
+
+  ngOnInit(): void {
+    const numbers$ = of(1, 2, 3);
+    numbers$.subscribe({
+      next: (value) =>
+        console.log('Observable emitted the next value: ' + value),
+      error: (err) => console.error('Observable emitted an error: ' + err),
+      complete: () =>
+        console.log('Observable emitted the complete notification'),
+    });
+  }
 }
